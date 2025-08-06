9debef007ef5cdc14c33923f747679a2eccd8b92
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-08-06 08:45:46 -0500

    new branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 // @ts-check
-import vercelAdapter from '@astrojs/vercel'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  output: 'server',
-  adapter: vercelAdapter(),
-  prefetch: true,
-})
+export default defineConfig({});
