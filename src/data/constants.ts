248e0a76fc3f9d304023733e19e336f15b4c1a0d
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-11-28 08:51:40 -0500

    feat: add navigation items for main sections of the portfolio

---
+++ src/data/constants.ts
@@ -0,0 +1,9 @@
+export const NAV_ITEMS = [
+  { title: "Inicio", href: "/" },
+  { title: "Sobre mí", href: "#about" },
+  { title: "Proyectos", href: "#projects" },
+  { title: "Contacto", href: "#contact" },
+  { title: "CV", href: "/resume" },
+  {title: "Recursos", href:"/resources"},
+  { title: "Fotos", href: "/photos" },
+];
